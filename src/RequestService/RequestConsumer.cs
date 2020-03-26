c929df67d4458f1cbe2b76ded1fb25d98bc61e07
Steliyan Stoyanov <steliyan@st6.io>
2020-03-26 11:21:44 +0200

    Remove ILog usage in RequestConsumer

--- src/RequestService/RequestConsumer.cs
+++ src/RequestService/RequestConsumer.cs
@@ -2,18 +2,14 @@
 {
     using System.Threading.Tasks;
     using MassTransit;
-    using MassTransit.Logging;
     using Sample.MessageTypes;
-
 
     public class RequestConsumer :
         IConsumer<ISimpleRequest>
     {
-        readonly ILog _log = Logger.Get<RequestConsumer>();
-
         public async Task Consume(ConsumeContext<ISimpleRequest> context)
         {
-            _log.InfoFormat("Returning name for {0}", context.Message.CustomerId);
+            System.Console.WriteLine("Returning name for {0}", context.Message.CustomerId);
 
             context.Respond(new SimpleResponse
             {
