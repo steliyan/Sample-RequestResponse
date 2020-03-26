baaea232ed99d769c1ddffd49da6b474955e2c46
Steliyan Stoyanov <steliyan@st6.io>
2020-03-26 11:15:53 +0200

    Remove log4net integration

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,25 +2,15 @@
 {
     using System;
     using System.Configuration;
-    using System.IO;
-    using System.Text;
     using System.Threading.Tasks;
-    using log4net.Config;
     using MassTransit;
-    using MassTransit.Log4NetIntegration.Logging;
     using MassTransit.Util;
     using Sample.MessageTypes;
-
 
     class Program
     {
         static void Main()
         {
-            ConfigureLogger();
-
-            // MassTransit to use Log4Net
-            Log4NetLogger.Use();
-
             IBusControl busControl = CreateBus();
 
             TaskUtil.Await(() => busControl.StartAsync());
@@ -72,27 +62,6 @@
                 h.Username("guest");
                 h.Password("guest");
             }));
-        }
-
-        static void ConfigureLogger()
-        {
-            const string logConfig = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
-<log4net>
-  <root>
-    <level value=""INFO"" />
-    <appender-ref ref=""console"" />
-  </root>
-  <appender name=""console"" type=""log4net.Appender.ColoredConsoleAppender"">
-    <layout type=""log4net.Layout.PatternLayout"">
-      <conversionPattern value=""%m%n"" />
-    </layout>
-  </appender>
-</log4net>";
-
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(logConfig)))
-            {
-                XmlConfigurator.Configure(stream);
-            }
         }
 
 
